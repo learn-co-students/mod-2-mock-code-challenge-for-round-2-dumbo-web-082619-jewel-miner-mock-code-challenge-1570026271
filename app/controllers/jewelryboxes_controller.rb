fdc05b4e2e8949ca3a11f67c81e0d199ad533402
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-10-02 12:41:23 -0400

    deliverables

--- app/controllers/jewelryboxes_controller.rb
+++ app/controllers/jewelryboxes_controller.rb
@@ -1,2 +1,31 @@
 class JewelryboxesController < ApplicationController
+
+    def new
+     @jewelrybox = Jewelrybox.new
+    end
+
+    
+    def create
+        @jewelrybox = Jewelrybox.create(sore)
+        if @jewelrybox.valid?
+           redirect_to miners_path
+        else
+        flash[:errors] = @jewelrybox.errors.full_messages
+        redirect_to new_jewelrybox_path
+        end
+    end
+
+
+
+    private
+
+    def doko
+        Miner.find(params[:id])
+    end
+
+    def sore
+        params.require(:jewelrybox).permit(:jewel_id, :miner_id)
+    end
+
+
 end
